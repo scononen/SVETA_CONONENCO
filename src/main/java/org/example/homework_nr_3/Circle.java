5179641efce20bb11ab76b4e5d55e9a57d61eec2
scononen <cononenco1277@gmail.com>
2023-04-02 20:39:26 +0300

    homework 3 with methods, constructor, objects

--- src/main/java/org/example/homework_nr_3/Circle.java
+++ src/main/java/org/example/homework_nr_3/Circle.java
@@ -2,29 +2,28 @@ package org.example.homework_nr_3;
 
 
 public class Circle {
-
     double radius;
     Circle(double radius) {
         this.radius = radius;
     }
 
-    public static double calculateArea(double Pi, double radius){
-             return Pi * radius * radius;
+    public static double calculateArea(double radius){
+             return Math.PI * radius * radius;
     }
 
     public static void main(String[] args){
 
-double PI =3.14;
 double radius =2.5;
 double radius1 = 47.3;
-        System.out.println("Площадь круга =" + calculateArea (PI,radius));
-//        System.out.println(calculateArea (3.14, 2.5));
+
+        System.out.println("Площадь круга =" + calculateArea(radius));
+//        System.out.println(calculateArea (2.5));
 
         Circle ABC = new Circle (radius1);
 
 
-        System.out.println("Площадь круга ="+ calculateArea(PI,ABC.radius));
-//        System.out.println(calculateArea (3.14, 47.3);
+        System.out.println("Площадь круга ="+ calculateArea(ABC.radius));
+//        System.out.println(calculateArea ( 47.3));
     }
 }
 
--- src/main/java/org/example/homework_nr_3/TemperatureConverter.java
+++ src/main/java/org/example/homework_nr_3/TemperatureConverter.java
@@ -3,29 +3,35 @@ package org.example.homework_nr_3;
 public class TemperatureConverter {
     double C;
     double F;
-    TemperatureConverter (double C, double F) {
+
+    TemperatureConverter(double C, double F) {
         this.C = C;
         this.F = F;
     }
-        public static double toCelsius(double F) {
-                return (F-32)*5/9;
-        }
-        public static double toFahrenheit(double C){
-                return C*9/5+32;
-        }
-        public static void main(String[] args){
+    public double toCelsius() {
+        return (F - 32) * 5 / 9;
+    }
 
-    double Celisia;  double Faregheit;
-            Celisia =28;  Faregheit =20;
+    public double toFahrenheit() {
+        return C * 9 / 5 + 32;
+    }
 
-            TemperatureConverter X= new TemperatureConverter (Celisia, Faregheit);
-            System.out.println("Градус Цельсия = " + Celisia + "  равен градусу  " + toFahrenheit(X.C) + "  Фаренгейта");
-            System.out.println("Градус Фаренгейта = " + Faregheit + "   равен градусу  " + toCelsius(X.F) + "  Цельсия");
+    public static void main(String[] args) {
+
+        double Celisia;
+        double Faregheit;
+        Celisia = 28;
+        Faregheit = 20;
+
+        TemperatureConverter X = new TemperatureConverter(Celisia, Faregheit);
+        System.out.println("Градус Цельсия = " + Celisia + "  равен градусу  " + X.toFahrenheit() + "  Фаренгейта");
+        System.out.println("Градус Фаренгейта = " + Faregheit + "   равен градусу  " + X.toCelsius() + "  Цельсия");
 
     }
 
 
-
-
-
 }
+
+
+
+
