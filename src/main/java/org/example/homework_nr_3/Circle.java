3e8536eac9ac719e7d48284d8b15595bd23067d9
scononen <cononenco1277@gmail.com>
2023-04-03 18:06:46 +0300

    homework 3 was removed static

--- src/main/java/org/example/homework_nr_3/Circle.java
+++ src/main/java/org/example/homework_nr_3/Circle.java
@@ -3,27 +3,27 @@ package org.example.homework_nr_3;
 
 public class Circle {
     double radius;
+
     Circle(double radius) {
         this.radius = radius;
     }
 
-    public static double calculateArea(double radius){
-             return Math.PI * radius * radius;
+    double calculateArea() {
+        return Math.PI * radius * radius;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 double radius =2.5;
 double radius1 = 47.3;
 
-        System.out.println("Площадь круга =" + calculateArea(radius));
-//        System.out.println(calculateArea (2.5));
 
-        Circle ABC = new Circle (radius1);
+        Circle ABC = new Circle (radius);
+        Circle X = new Circle(radius1);
 
+        System.out.println("Площадь круга с радиусом " +radius + " равна " + ABC.calculateArea());
+        System.out.println("Площадь круга с радиусом "+ radius1+ " равна " + X.calculateArea());
 
-        System.out.println("Площадь круга ="+ calculateArea(ABC.radius));
-//        System.out.println(calculateArea ( 47.3));
     }
 }
 
